892347b8b740d8ce23fca17ac48374e7192bf3d5
Michael Bryzek <mbryzek@alum.mit.edu>
2016-02-11 16:28:43 -0500

    lib compiles

--- lib/src/test/scala/UrlsSpec.scala
+++ lib/src/test/scala/UrlsSpec.scala
@@ -1,6 +1,6 @@
 package io.flow.delta.lib
 
-import io.flow.delta.v0.models.{ItemSummaryUndefinedType, RecommendationType}
+import io.flow.delta.v0.models.ItemSummaryUndefinedType
 import org.scalatest.{FunSpec, Matchers}
 
 class UrlsSpec extends FunSpec with Matchers with Factories {
@@ -11,24 +11,7 @@ class UrlsSpec extends FunSpec with Matchers with Factories {
     urls.www("/foo") should be("http://localhost/foo")
   }
 
-  it("recommendation") {
-    val binary = makeRecommendation(`type` = RecommendationType.Binary)
-    urls.recommendation(binary) should be(s"/binaries/${binary.`object`.id}")
-
-    val library = makeRecommendation(`type` = RecommendationType.Library)
-    urls.recommendation(library) should be(s"/libraries/${library.`object`.id}")
-
-    val other = makeRecommendation(`type` = RecommendationType.UNDEFINED("other"))
-    urls.recommendation(other) should be("#")
-  }
-
   it("itemSummary") {
-    val binary = makeBinarySummary()
-    urls.itemSummary(binary) should be(s"/binaries/${binary.id}")
-
-    val library = makeLibrarySummary()
-    urls.itemSummary(library) should be(s"/libraries/${library.id}")
-
     val project = makeProjectSummary()
     urls.itemSummary(project) should be(s"/projects/${project.id}")
 
