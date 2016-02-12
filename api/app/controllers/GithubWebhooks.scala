dc1536f1e21993517ff592b945516d07c08949b7
Michael Bryzek <mbryzek@alum.mit.edu>
2016-02-11 21:34:44 -0500

    Cleanup imports

--- api/app/controllers/GithubWebhooks.scala
+++ api/app/controllers/GithubWebhooks.scala
@@ -1,21 +1,13 @@
 package controllers
 
-import io.flow.common.v0.models.json._
 import io.flow.delta.actors.MainActor
-import io.flow.delta.v0.models.json._
 import io.flow.postgresql.Authorization
 import db.ProjectsDao
-import io.flow.play.clients.UserTokensClient
-import io.flow.play.controllers.IdentifiedRestController
-import io.flow.play.util.Validation
-import io.flow.postgresql.Pager
 import play.api.mvc._
 import play.api.libs.json._
 
 @javax.inject.Singleton
-class GithubWebhooks @javax.inject.Inject() (
-  val userTokensClient: UserTokensClient
-) extends Controller with IdentifiedRestController with Helpers {
+class GithubWebhooks @javax.inject.Inject() () extends Controller {
 
   def postByProjectId(projectId: String) = Action { request =>
     ProjectsDao.findById(Authorization.All, projectId) match {
