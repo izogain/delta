2ecb294f630829c46330975478aca36f2bc6bf99
Michael Bryzek <mbryzek@alum.mit.edu>
2016-02-13 12:58:21 -0500

    Connect supervisor to event log

--- api/app/lib/EventLog.scala
+++ api/app/lib/EventLog.scala
@@ -3,6 +3,8 @@ package io.flow.delta.api.lib
 import db.UsersDao
 import io.flow.delta.v0.models.Project
 import io.flow.common.v0.models.User
+import java.io.{PrintWriter, StringWriter}
+import org.joda.time.DateTime
 import scala.util.{Failure, Success, Try}
 
 object EventLog {
@@ -22,9 +24,32 @@ case class EventLog(
   prefix: String
 ) {
 
-  def started(message: String) = {}
-  def completed(message: String, error: Option[Throwable] = None) = {}
-  def running(message: String) = {}
+  def started(message: String) = {
+    println(format(s"started $message"))
+  }
+
+  def completed(message: String, error: Option[Throwable] = None) = {
+    error match {
+      case None => {
+        println(format(s"completed $message"))
+      }
+      case Some(ex) => {
+        // this works much better
+        val sw = new StringWriter
+        ex.printStackTrace(new PrintWriter(sw))
+        println(format(s"error $message: ${ex.getMessage}\n\n$sw"))
+      }
+    }
+  }
+
+  def running(message: String) = {
+    println(format(s"running $message"))
+  }
+
+  private[this] def format(message: String): String = {
+    val ts = new DateTime()
+    s"[$ts] ${project.id} $message"
+  }
 
   def run(
     message: String
