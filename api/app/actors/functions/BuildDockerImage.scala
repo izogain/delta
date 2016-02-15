2a3883d085f3570cf62a25aae84b41c2f21e0cfa
Michael Bryzek <mbryzek@alum.mit.edu>
2016-02-15 12:29:09 -0500

    Select docker images to build based on expected state

--- api/app/actors/functions/BuildDockerImage.scala
+++ api/app/actors/functions/BuildDockerImage.scala
@@ -1,9 +1,8 @@
 package io.flow.delta.actors.functions
 
-import db.{ImagesDao, TagsDao, UsersDao}
+import db.{ImagesDao, ProjectExpectedStatesDao}
 import io.flow.delta.actors.{MainActor, EventLog, SupervisorFunction, SupervisorResult}
 import io.flow.postgresql.Authorization
-import io.flow.delta.api.lib.GithubUtil
 import io.flow.delta.v0.models.{Project, Settings}
 import play.api.Logger
 import play.libs.Akka
@@ -12,9 +11,10 @@ import scala.concurrent.Future
 import scala.util.{Failure, Success, Try}
 
 /**
-  * Looks up the latest tag for a project. If found, checks to see if
-  * we already have a docker image locally for that tag. If not,
-  * ensures that there is a docker image being built for it.
+  * Looks up the expected state for a project. If found, checks to see
+  * if we already have a docker image locally for each version in the
+  * expected stated, triggering docker builds for each image that is
+  * not found locally.
   */
 object BuildDockerImage extends SupervisorFunction {
 
@@ -40,26 +40,37 @@ case class BuildDockerImage(project: Project) extends Github with EventLog {
     Some(f(project))
   }
 
-  private[this] val repo = GithubUtil.parseUri(project.uri).right.getOrElse {
-    sys.error(s"Project id[${project.id}] uri[${project.uri}]: Could not parse")
-  }
-
   def run(
     implicit ec: scala.concurrent.ExecutionContext
   ): SupervisorResult = {
-    TagsDao.findLatestByProjectId(Authorization.All, project.id) match {
+    ProjectExpectedStatesDao.findByProjectId(Authorization.All, project.id) match {
       case None => {
-        SupervisorResult.NoChange("Project does not have any tags")
+        SupervisorResult.NoChange("Project does not have an expected state")
       }
 
-      case Some(tag) => {
-        ImagesDao.findByProjectIdAndVersion(project.id, tag.name) match {
-          case Some(i) => {
-            SupervisorResult.NoChange(s"Image ${repo}:${tag.name} already exist")
+      case Some(state) => {
+        val versions = state.versions.flatMap { version =>
+          println(s"version[$version]")
+          ImagesDao.findByProjectIdAndVersion(project.id, version.name) match {
+            case Some(i) => {
+              None
+            }
+            case None => {
+              MainActor.ref ! MainActor.Messages.BuildDockerImage(project.id, version.name)
+              Some(version.name)
+            }
           }
-          case None => {
-            MainActor.ref ! MainActor.Messages.BuildDockerImage(project.id, tag.name)
-            SupervisorResult.Change(s"Started build of docker image ${repo}:${tag.name}")
+        }
+
+        versions.toList match {
+          case Nil => {
+            SupervisorResult.NoChange(s"All images exist for versions in expected state[%s]".format(state.versions.map(_.name).mkString(", ")))
+          }
+          case one :: Nil => {
+            SupervisorResult.Change(s"Started build of docker image for version $one")
+          }
+          case multiple => {
+            SupervisorResult.Change(s"Started build of docker images for versions %s".format(multiple.mkString(", ")))
           }
         }
       }
