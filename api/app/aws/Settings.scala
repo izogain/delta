55917ba4215dc1fb45fe551357e5ca923806deb1
Paolo Lim <paolo@flow.io>
2016-02-17 10:41:21 -0500

    Update AWS settings for Flow account

--- api/app/aws/Settings.scala
+++ api/app/aws/Settings.scala
@@ -10,16 +10,18 @@ trait Settings {
   val asgHealthCheckGracePeriod = 300
 
   // The minimum size of the Auto Scaling group
-  val asgMinSize = 3
+  val asgMinSize = 4
 
   // The maximum size of the Auto Scaling group
-  val asgMaxSize = 3
+  val asgMaxSize = 4
 
   // The number of Amazon EC2 instances that should be running in the autoscaling group
-  val asgDesiredSize = 3
+  val asgDesiredSize = 4
 
   // Subnets for the load balancer and autoscaling group instances
-  val subnets = Seq("subnet-719c7029", "subnet-3538f243", "subnet-2338f255", "subnet-6c9c7034")
+  // subnet-2338f255 = production-private-us-east-1a
+  // subnet-719c7029 = production-private-us-east-1b
+  val subnets = Seq("subnet-719c7029", "subnet-2338f255")
 
   // Keypair name used to SSH into EC2 instances created by the autoscaling group
   val ec2KeyName = "flow-admin"
@@ -31,10 +33,12 @@ trait Settings {
   val launchConfigIamInstanceProfile = "ecsInstanceRole"
 
   // Security groups for the EC2 instances launch configuration and autoscaling group
-  val securityGroups = Seq("sg-47fb1b3e")
+  // sg-aa26c6d3 = ssh from bastion
+  // sg-47fb1b3e = all traffic from within production vpc
+  val securityGroups = Seq("sg-47fb1b3e", "sg-aa26c6d3")
 
   // Should this be higher? And if so, probably should use something other than t2.micro
-  val containerMemory = 1024
+  val containerMemory = 500
 
   // Launch configuration EC2 instance type. t2 micro for now, but probably change later or make configurable
   val launchConfigInstanceType = "t2.micro"
