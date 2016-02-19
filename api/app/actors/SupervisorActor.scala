38430b17aafa0dfc05ef095adf9a3cf9e800cfd2
Michael Bryzek <mbryzek@alum.mit.edu>
2016-02-19 09:55:27 -0500

    Remove await in supervisor

--- api/app/actors/SupervisorActor.scala
+++ api/app/actors/SupervisorActor.scala
@@ -6,9 +6,6 @@ import io.flow.delta.v0.models.{Project, Settings}
 import io.flow.play.actors.Util
 import io.flow.postgresql.Authorization
 import play.libs.Akka
-import scala.concurrent.Await
-import scala.concurrent.duration.Duration
-import scala.util.{Failure, Success, Try}
 
 object SupervisorActor {
 
@@ -97,31 +94,22 @@ class SupervisorActor extends Actor with Util with DataProject with EventLog {
           }
           case true => {
             log.started(format(f))
-            Try(
-              // TODO: Remove the await
-              Await.result(
-                f.run(project),
-                Duration(10, "minutes")
-              )
-            ) match {
-              case Success(result) => {
-                result match {
-                  case SupervisorResult.Change(desc) => {
-                    log.changed(format(f, desc))
-                  }
-                  case SupervisorResult.NoChange(desc)=> {
-                    log.completed(format(f, desc))
-                    run(project, settings, functions.drop(1))
-                  }
-                  case SupervisorResult.Error(desc, ex)=> {
-                    log.completed(format(f, desc), Some(ex))
-                  }
+            f.run(project).map { result =>
+              result match {
+                case SupervisorResult.Change(desc) => {
+                  log.changed(format(f, desc))
+                }
+                case SupervisorResult.NoChange(desc)=> {
+                  log.completed(format(f, desc))
+                  run(project, settings, functions.drop(1))
+                }
+                case SupervisorResult.Error(desc, ex)=> {
+                  log.completed(format(f, desc), Some(ex))
                 }
               }
 
-              case Failure(ex) => {
-                log.completed(format(f, ex.getMessage), Some(ex))
-              }
+            }.recover {
+              case ex: Throwable => log.completed(format(f, ex.getMessage), Some(ex))
             }
           }
         }
