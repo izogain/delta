8f2a61afbcb91f33822ceb76e4db4ff0bfa22a60
Michael Bryzek <mbryzek@alum.mit.edu>
2016-02-20 14:10:21 -0500

    Displays versions always sorted

--- api/app/lib/StateFormatter.scala
+++ api/app/lib/StateFormatter.scala
@@ -6,10 +6,17 @@ import io.flow.delta.v0.models.Version
 object StateFormatter {
 
   def label(versions: Seq[Version]): String = {
-    versions.map { v =>
-      val label = Text.pluralize(v.instances, "instance", "instances")
-      s"${v.name}: $label"
-    }.mkString(", ")
+    versions.
+      sortBy { v =>
+        Semver.parse(v.name) match {
+          case None => s"9:$v"
+          case Some(tag) => s"1:${tag.sortKey}"
+        }
+      }.
+      map { v =>
+        val label = Text.pluralize(v.instances, "instance", "instances")
+        s"${v.name}: $label"
+      }.mkString(", ")
   }
 
 }
