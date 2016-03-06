3a8923f8b514ab2b2b1ef6864de0ebd4ea2519c3
Michael Bryzek <mbryzek@alum.mit.edu>
2016-03-06 13:03:59 -0500

    Cleanup display of dashboard

--- www/app/controllers/ApplicationController.scala
+++ www/app/controllers/ApplicationController.scala
@@ -3,9 +3,49 @@ package controllers
 import io.flow.delta.www.lib.DeltaClientProvider
 import io.flow.play.clients.UserTokensClient
 import io.flow.play.util.{Pagination, PaginatedCollection}
+import org.joda.time.DateTime
 import play.api._
 import play.api.i18n.MessagesApi
 import play.api.mvc._
+
+/**
+  * Wrapper to simplify display
+  */
+case class BuildView(val dashboardBuild: io.flow.delta.v0.models.DashboardBuild) {
+
+  private[this] val MinutesUntilError = 30
+
+  private[this] val last = dashboardBuild.last.versions.map(_.name).mkString(", ")
+  private[this] val desired = dashboardBuild.desired.versions.map(_.name).mkString(", ")
+
+  val status: Option[String] = {
+    last == desired match {
+      case true => None
+      case false => {
+        Some(
+          if (dashboardBuild.desired.timestamp.isBefore(new DateTime().minusMinutes(MinutesUntilError))) {
+            "danger"
+          } else {
+            "warning"
+          }
+        )
+      }
+    }
+  }
+
+  val label = {
+    last == desired match {
+      case true => {
+        s"Running $desired"
+      }
+      case false => {
+        s"Transitioning from $last to $desired"
+      }
+    }
+
+  }
+
+}
 
 class ApplicationController @javax.inject.Inject() (
   val messagesApi: MessagesApi,
@@ -31,7 +71,7 @@ class ApplicationController @javax.inject.Inject() (
       Ok(
         views.html.index(
           uiData(request).copy(organization = organization),
-          PaginatedCollection(buildsPage, dashboardBuilds)
+          PaginatedCollection(buildsPage, dashboardBuilds.map(BuildView(_)))
         )
       )
     }
