69800f777e810255902547f1b753604507b12a92
Paolo Lim <paolo@flow.io>
2016-04-29 11:28:55 -0400

    Update ecs memory setting for t2.micro (#188)

--- api/app/aws/InstanceType.scala
+++ api/app/aws/InstanceType.scala
@@ -4,7 +4,9 @@ case class InstanceType(launchConfigInstanceType: String, containerMemory: Int) 
 
 object InstanceTypes {
 
-  val T2Micro = InstanceType("t2.micro", 400)
+  // see https://aws.amazon.com/ec2/instance-types/ for maximums
+  // InstanceType takes an amazon instance type name and container memory in MBs
+  val T2Micro = InstanceType("t2.micro", 700)
   val T2Small = InstanceType("t2.small", 1500)
   val T2Medium = InstanceType("t2.medium", 3500)
   val T2Large = InstanceType("t2.large", 7500)
@@ -12,4 +14,3 @@ object InstanceTypes {
   val M4Large = InstanceType("m4.large", 7500)
 
 }
-
