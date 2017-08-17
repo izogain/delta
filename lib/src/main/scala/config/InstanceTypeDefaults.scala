bdd545dbd2657684c2294988a42b0ce203046146
Paolo Lim <paolo@flow.io>
2017-08-17 10:41:35 -0400

    Autocalculate memory allocation to jvm based on real memory available in ec2 instance (#159)

--- lib/src/main/scala/config/InstanceTypeDefaults.scala
+++ lib/src/main/scala/config/InstanceTypeDefaults.scala
@@ -5,20 +5,23 @@ import play.api.Logger
 
 object InstanceTypeDefaults {
 
+  // see for memory settings (in GB): https://aws.amazon.com/ec2/instance-types/
   def memory(typ: InstanceType): Int = {
+    // max memory for each instance type in MB
+    // TODO: must be a way to get these programmatically
     typ match {
-      case InstanceType.M4Large => 7500
-      case InstanceType.M4Xlarge => 15500
-      case InstanceType.M42xlarge => 31500
+      case InstanceType.M4Large => 8000
+      case InstanceType.M4Xlarge => 16000
+      case InstanceType.M42xlarge => 32000
 
-      case InstanceType.C4Large => 3500
-      case InstanceType.C4Xlarge => 7200
-      case InstanceType.C42xlarge => 14700
+      case InstanceType.C4Large => 3750
+      case InstanceType.C4Xlarge => 7500
+      case InstanceType.C42xlarge => 15000
 
-      case InstanceType.T2Micro => 700
-      case InstanceType.T2Small => 1500
-      case InstanceType.T2Medium => 3500
-      case InstanceType.T2Large => 7500
+      case InstanceType.T2Micro => 1000
+      case InstanceType.T2Small => 2000
+      case InstanceType.T2Medium => 4000
+      case InstanceType.T2Large => 8000
 
       case InstanceType.UNDEFINED(other) => {
         Logger.warn(s"Undefined instance type[$other]. Using default memory setting")
