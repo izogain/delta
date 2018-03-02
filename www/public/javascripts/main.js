541b147bbfbdfb4255c7297b28b679aa1e87fe70
Ben Iofel <iofelben+github@gmail.com>
2018-03-02 12:12:13 -0500

    main.js fixes (#249)
    * fix main.js
    
    * format main.js
    
    * remove duplicate line

---
+++ www/public/javascripts/main.js
@@ -0,0 +1,22 @@
+$(function () {
+    $('.setting').on('change', 'input[type="radio"]', function (e) {
+        console.log('tried to change a setting!');
+        var input = e.target;
+        var name = input.name;
+        var value = input.id;
+        var project = Delta.project.id;
+        var data = {};
+
+        data[name] = value === "Enabled";
+
+        $.post(project + '/settings', data, function () {
+            // show flash - TBD
+        });
+    });
+
+    if (window.location.search.indexOf('autorefresh') !== -1) {
+        setTimeout(function () {
+            window.location.reload();
+        }, 5000);
+    }
+});
