3ef60c08aff70810e2fbfb4e51766049583f45ce
michael-palma <michael.palma.1024@gmail.com>
2018-04-26 12:01:45 -0400

    Adding build arguments for natero API and AUTH keys (#285)

--- api/app/actors/functions/TravisCiBuild.scala
+++ api/app/actors/functions/TravisCiBuild.scala
@@ -112,7 +112,7 @@ class TravisCiDockerImageBuilder @Inject()(
   private def postBuildRequest(travisCiBuild: TravisCiBuild, client: Client) {
     val dockerImageName = BuildNames.dockerImageName(travisCiBuild.org.docker, travisCiBuild.build)
     val projectId = travisCiBuild.project.id
-    
+
     try {
 
       val response = client.requests.post(
@@ -167,7 +167,7 @@ class TravisCiDockerImageBuilder @Inject()(
           script = Option(Seq(
             "docker --version",
             "echo TRAVIS_BRANCH=$TRAVIS_BRANCH",
-            s"docker build --build-arg NPM_TOKEN=$${NPM_TOKEN} --build-arg AWS_ACCESS_KEY_ID=$${AWS_ACCESS_KEY_ID} --build-arg AWS_SECRET_ACCESS_KEY=$${AWS_SECRET_ACCESS_KEY} -f ${travisCiBuild.buildConfig.dockerfile} -t ${dockerImageName}:$${TRAVIS_BRANCH} .",
+            s"docker build --build-arg NPM_TOKEN=$${NPM_TOKEN} --build-arg AWS_ACCESS_KEY_ID=$${AWS_ACCESS_KEY_ID} --build-arg AWS_SECRET_ACCESS_KEY=$${AWS_SECRET_ACCESS_KEY} --build-arg NATERO_API_KEY=$${NATERO_API_KEY} --build-arg NATERO_AUTH_KEY=$${NATERO_AUTH_KEY} -f ${travisCiBuild.buildConfig.dockerfile} -t ${dockerImageName}:$${TRAVIS_BRANCH} .",
             "docker login -u=$DOCKER_USERNAME -p=$DOCKER_PASSWORD",
             s"docker push ${dockerImageName}:$${TRAVIS_BRANCH}"
           )),
